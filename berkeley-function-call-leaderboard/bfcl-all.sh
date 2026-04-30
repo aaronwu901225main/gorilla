ad8f41b4512405095a38d1530c2356913613df79
aaronwu901225main <aaronwu901225main@gmail.com>
2026-04-30 14:46:35 +0800

    New results

--- berkeley-function-call-leaderboard/bfcl-all.sh
+++ berkeley-function-call-leaderboard/bfcl-all.sh
@@ -9,8 +9,8 @@ JOB3_SCRIPT="bfcl-gen-gptoss-merged-3.slurm"
 # JOB4_SCRIPT="bfcl-gen-gptoss-merged-4.slurm"
 # JOB5_SCRIPT="bfcl-gen-gptoss-merged-5.slurm"
 
-#JOB1_SCRIPT="bfcl-gen-gemma4-fc-1.slurm"
-#JOB2_SCRIPT="bfcl-gen-gemma4-fc-2.slurm"
+JOB4_SCRIPT="bfcl-gen-gemma4-fc-1.slurm"
+JOB5_SCRIPT="bfcl-gen-gemma4-fc-2.slurm"
 #JOB3_SCRIPT="bfcl-gen-gemma4-fc-3.slurm"
 #JOB4_SCRIPT="bfcl-gen-gemma4-fc-4.slurm"
 
@@ -20,8 +20,8 @@ JOB_SCRIPTS=(
   "$JOB1_SCRIPT"
   "$JOB2_SCRIPT"
   "$JOB3_SCRIPT"
-  # "$JOB4_SCRIPT"
-#  "$JOB5_SCRIPT"
+  "$JOB4_SCRIPT"
+  "$JOB5_SCRIPT"
 )
 
 job_ids=()
