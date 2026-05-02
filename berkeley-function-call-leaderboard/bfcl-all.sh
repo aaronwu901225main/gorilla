272bebb0c5c303437b05f61aa06ce7585ae39255
aaronwu901225main <aaronwu901225main@gmail.com>
2026-05-02 08:33:18 +0800

    New results

--- berkeley-function-call-leaderboard/bfcl-all.sh
+++ berkeley-function-call-leaderboard/bfcl-all.sh
@@ -3,13 +3,13 @@ conda activate BFCL
 set -euo pipefail
 
 # JOB1_SCRIPT="bfcl-gen.slurm"
-JOB1_SCRIPT="bfcl-gen-gptoss-merged-1.slurm"
-JOB2_SCRIPT="bfcl-gen-gptoss-merged-2.slurm"
-JOB3_SCRIPT="bfcl-gen-gptoss-merged-3.slurm"
-JOB4_SCRIPT="bfcl-gen-gptoss-merged-4.slurm"
+# JOB1_SCRIPT="bfcl-gen-gptoss-merged-1.slurm"
+# JOB2_SCRIPT="bfcl-gen-gptoss-merged-2.slurm"
+# JOB3_SCRIPT="bfcl-gen-gptoss-merged-3.slurm"
+# JOB4_SCRIPT="bfcl-gen-gptoss-merged-4.slurm"
 # JOB5_SCRIPT="bfcl-gen-gptoss-merged-5.slurm"
 
-# JOB1_SCRIPT="bfcl-gen-gemma4-fc-1.slurm"
+JOB1_SCRIPT="bfcl-gen-gemma4-fc-1.slurm"
 # JOB2_SCRIPT="bfcl-gen-gemma4-fc-2.slurm"
 #JOB3_SCRIPT="bfcl-gen-gemma4-fc-3.slurm"
 #JOB4_SCRIPT="bfcl-gen-gemma4-fc-4.slurm"
@@ -18,9 +18,9 @@ JOB4_SCRIPT="bfcl-gen-gptoss-merged-4.slurm"
 
 JOB_SCRIPTS=(
   "$JOB1_SCRIPT"
-  "$JOB2_SCRIPT"
-  "$JOB3_SCRIPT"
-  "$JOB4_SCRIPT"
+  # "$JOB2_SCRIPT"
+  # "$JOB3_SCRIPT"
+  # "$JOB4_SCRIPT"
   # "$JOB5_SCRIPT"
 )
 
