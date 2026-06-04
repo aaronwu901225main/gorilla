129e7cf3ea3a289716600a844ef520f5f732eb6c
aaronwu901225main <aaronwu901225main@gmail.com>
2026-06-04 16:27:56 +0800

    New results

--- berkeley-function-call-leaderboard/bfcl-all.sh
+++ berkeley-function-call-leaderboard/bfcl-all.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 # DEPENDENCY=55953
 # JOB1_SCRIPT="bfcl-gen.slurm"
 JOB1_SCRIPT="bfcl-gen-gptoss-merged-1.slurm"
-JOB2_SCRIPT="bfcl-gen-gptoss-merged-2.slurm"
+# JOB2_SCRIPT="bfcl-gen-gptoss-merged-2.slurm"
 JOB3_SCRIPT="bfcl-gen-gptoss-merged-3.slurm"
-# JOB4_SCRIPT="bfcl-gen-gptoss-merged-4.slurm"
+JOB4_SCRIPT="bfcl-gen-gptoss-merged-4.slurm"
 # JOB5_SCRIPT="bfcl-gen-gptoss-merged-5.slurm"
 
 # JOB1_SCRIPT="bfcl-gen-gemma4-fc-1.slurm"
@@ -18,9 +18,9 @@ JOB3_SCRIPT="bfcl-gen-gptoss-merged-3.slurm"
 
 JOB_SCRIPTS=(
   "$JOB1_SCRIPT"
-  "$JOB2_SCRIPT"
+  # "$JOB2_SCRIPT"
   "$JOB3_SCRIPT"
-  # "$JOB4_SCRIPT"
+  "$JOB4_SCRIPT"
   # "$JOB5_SCRIPT"
 )
 
